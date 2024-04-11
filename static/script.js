563f98a952b47e03e4444fbc93e0039b71c3111e
xtelos <df4182@gmail.com>
2024-04-10 22:39:21 -0400

    removed favoriting feature

--- static/script.js
+++ static/script.js
@@ -4,94 +4,47 @@
 document.getElementById('game').addEventListener('submit', function(e) {
     e.preventDefault();
     var gameName = document.getElementsByName('gameName')[0].value;
-    var xhr = new XMLHttpRequest();
-    var url = '/games/' + gameName;
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            var data = JSON.parse(xhr.responseText);
-            var steam_price = data['price'];
-            var gameData = '<p>' + gameName + ' is ' + steam_price + ' on the Steam store</p>';
-            document.getElementById('rawData1').innerHTML = gameData;
-            document.getElementById('rawData1').style.display = 'inline-block';
+    fetch('/games/' + gameName, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
         }
-    };
-    xhr.send();
+    })
+    .then(response => response.json())
+    .then(data => {
+        var steam_price = data['price'];
+        var gameData = '<p>' + gameName + ' is ' + steam_price + ' on the Steam store</p>';
+        document.getElementById('rawData1').innerHTML = gameData;
+        document.getElementById('rawData1').style.display = 'inline-block';
+    });
 });
+
+
 // add news for the game searched from flask
 document.getElementById('game').addEventListener('submit', function(e) {
     e.preventDefault();
     var gameName = document.getElementsByName('gameName')[0].value;
-    var xhr = new XMLHttpRequest();
-    var url = '/gamedata/' + gameName;
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            var data = JSON.parse(xhr.responseText);
-            var news = '<p>News for ' + gameName + '</p>';
-            document.getElementById('rawData2').innerHTML = news;
-            var newsList = document.createElement('ul');
-            document.getElementById('rawData2').appendChild(newsList);
-            for (var i = 0; i < data.length; i++) {
-                var news = '<li>' + data[i] + '</li>';
-                newsList.innerHTML += news;
-                var line_break = document.createElement('br');
-                newsList.appendChild(line_break);
-            }
-            document.getElementById('rawData2').style.display = 'inline-block';
-            var favoriteButton = document.getElementById('favoriteButton')
-            favoriteButton.style.display = 'inline';
-            checkIfFavorited(gameName)
+    fetch('/gamedata/' + gameName, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
         }
-    };
-    xhr.send();
+    })
+    .then(response => response.json())
+    .then(data => {
+        var news = '<p>News for ' + gameName + '</p>';
+        document.getElementById('rawData2').innerHTML = news;
+        var newsList = document.createElement('ul');
+        document.getElementById('rawData2').appendChild(newsList);
+        for (var i = 0; i < data.length; i++) {
+            var news = '<li>' + data[i] + '</li>';
+            newsList.innerHTML += news;
+            var line_break = document.createElement('br');
+            newsList.appendChild(line_break);
+        }
+        document.getElementById('rawData2').style.display = 'inline-block';
+        var favoriteButton = document.getElementById('favoriteButton')
+        favoriteButton.style.display = 'inline';
+        checkIfFavorited(gameName)
+    });
 });
-
-// Adds game to favorites
-function change()
-{
-    var elem = document.getElementById("favoriteButton");
-    var gameName = document.getElementsByName('gameName')[0].value;
-    if (elem.value=="Remove from Favorites") {
-        var xhr = new XMLHttpRequest();
-        var url = '/remove_favorite/' + gameName;
-        xhr.open('POST', url, true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-                elem.value = "Add to Favorites";}
-            else elem.value = xhr.status;
-        }; 
-        xhr.send();
-    }
-    else {
-        var xhr = new XMLHttpRequest();
-        var url = '/add_favorite/' + gameName;
-        xhr.open('POST', url, true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-                elem.value = "Remove from Favorites";}
-            else elem.value = xhr.status;
-        }; 
-        xhr.send();
-    }
-}
-
-function checkIfFavorited(gameName)
-{
-    var elem = document.getElementById("favoriteButton");
-    var xhr = new XMLHttpRequest();
-    var url = '/check_favorite/' + gameName;
-    xhr.open('GET', url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            if (xhr.responseText == "True")
-                elem.value = "Remove from Favorites";}
-            else elem.value = "Add to Favorites";
-    }; 
-    xhr.send();
-}
